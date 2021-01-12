c3088fde6edc8e8e2fdeadb7399517590905aaad
seowonjune <wonjuneprog@gmail.com>
2021-01-13 05:42:32 +0900

    message

--- api.py
+++ api.py
@@ -22,5 +22,15 @@ print(image_name + " dwonloading complete")
 img_color = cv2.imread(image_name, cv2.IMREAD_COLOR)
 
 cv2.imshow(image_name, img_color)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+#making edge
+src = cv2.imread(image_name, cv2.IMREAD_COLOR)
+
+canny = cv2.Canny(src, 310, 255)
+
+cv2.imshow("canny", canny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
