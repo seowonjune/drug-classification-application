05cdc4e8abdc73d361df67d78db9b2d3a1f085e0
seowonjune <wonjuneprog@gmail.com>
2021-01-13 12:19:21 +0900

    message

--- api.py
+++ api.py
@@ -3,8 +3,12 @@ import time
 import os
 from PIL import Image
 import cv2 
+import pandas as pd
 
 #loading execl file
+drug_name = pd.read_excel(r'C:\Users\swj35\OneDrive\바탕 화면\drug-classification-application\공공데이터개방_낱알식별목록.xlsx', usecols=[2]) #열 읽기, skiprows=[]는 행 읽기
+
+drug_url = pd.read_excel(r'C:\Users\swj35\OneDrive\바탕 화면\drug-classification-application\공공데이터개방_낱알식별목록.xlsx', usecols=[5])
 
 #appointing url & name of image
 url = "https://nedrug.mfds.go.kr/pbp/cmn/itemImageDownload/1Muwq7fAuBq"
@@ -44,7 +48,7 @@ cv2.imshow("canny", canny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#saving - made edge - jpg
+# saving - made edge - jpg
 # path = r'C:\Users\swj35\OneDrive\바탕 화면\drug-classification-application\edge'
 # #image_name_path = image_name + "_path"
 # cv2.imwrite(os.path.join(path , "test_edge.jpg"), canny)
@@ -52,3 +56,5 @@ cv2.destroyAllWindows()
 cv2.imwrite('test_edge.jpg', canny)
 
 print("test_edge.jpg download compeleted")
+
+#OCR with Tesseract
